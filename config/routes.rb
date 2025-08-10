7239412d6ef039349330322d20373a7c10fa1b3d
raja <rajadodve43@gmail.com>
2025-08-10 17:54:39 +0000

    Remove explore

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get "explore/index"
-  get "profiles/show"
   devise_for :users
 
   root "media_items#index"
@@ -13,8 +11,6 @@ Rails.application.routes.draw do
       patch :decrement_chapter
     end
   end
-
-  get "explore", to: "explore#index"
 
   resources :profiles, only: [ :show ]
 end
