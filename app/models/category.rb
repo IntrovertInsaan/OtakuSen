9edd802f1b21b3b2c407f51e3a3f42ff46b5b89e
raja <rajadodve43@gmail.com>
2025-08-10 06:44:49 +0000

    Add association, validations between Category and MediaItem models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+  has_many :media_items, dependent: :destroy
+  validates :name, presence: true, uniqueness: true
 end
--- app/models/media_item.rb
+++ app/models/media_item.rb
@@ -1,3 +1,6 @@
 class MediaItem < ApplicationRecord
   belongs_to :category
+
+  validates :title, presence: true
+  validates :status, presence: true
 end
