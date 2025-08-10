8b48b19e68a4fb8973beaa205359a9743ac15773
raja <rajadodve43@gmail.com>
2025-08-10 18:42:18 +0000

    Add not found page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,18 +1,16 @@
 class ProfilesController < ApplicationController
   def show
     @user = User.find(params[:id])
-
-    # Load all categories to display as filter links
     @categories = Category.all.order(:name)
-
-    # Start with the specific user's media items
     @media_items = @user.media_items
 
-    # If a category_id is present in the URL, filter by it
     if params[:category_id].present?
       @media_items = @media_items.where(category_id: params[:category_id])
     end
 
     @media_items = @media_items.order(created_at: :desc)
+
+  rescue ActiveRecord::RecordNotFound
+    render "profiles/not_found", status: :not_found
   end
 end
