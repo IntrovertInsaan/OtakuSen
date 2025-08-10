af21d04f1354c06fe115502730155968aa669ee9
raja <rajadodve43@gmail.com>
2025-08-10 17:51:20 +0000

    Add pagination in explorer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :username ])
     devise_parameter_sanitizer.permit(:account_update, keys: [ :username ])
   end
+
+  include Pagy::Backend
 end
--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -2,16 +2,15 @@ class ExploreController < ApplicationController
   def index
     @categories = Category.all.order(:name)
 
-    @media_items = MediaItem.all
+    all_items = MediaItem.all
 
     if params[:category_id].present?
-      @media_items = @media_items.where(category_id: params[:category_id])
+      all_items = all_items.where(category_id: params[:category_id])
     end
-
     if params[:search].present?
-      @media_items = @media_items.search_by_title_and_description(params[:search])
+      all_items = all_items.search_by_title_and_description(params[:search])
     end
 
-    @media_items = @media_items.order(created_at: :desc)
+    @pagy, @media_items = pagy(all_items.order(created_at: :desc))
   end
 end
