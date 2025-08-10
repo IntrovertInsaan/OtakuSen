73ec47db88c4cf41d9dc4e481150d38aeddf00b7
raja <rajadodve43@gmail.com>
2025-08-10 06:53:23 +0000

    Seed initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Destroying existing MediaItems and Categories..."
+MediaItem.destroy_all
+Category.destroy_all
+
+puts "Creating Categories..."
+
+categories = [
+  "Manga", "Manhwa", "Manhua", "Light Novel", "Web Novel", "Anime",
+  "Movies", "TV Series", "Web Series", "Documentary", "Song"
+]
+
+categories.each do |cat_name|
+  Category.find_or_create_by!(name: cat_name)
+end
+
+puts "Finished seeding categories!"
+puts "Created #{Category.count} categories."
