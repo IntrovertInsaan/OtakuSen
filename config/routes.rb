29a16c24278d1b0eef14661e7cda0f5bb21464d9
raja <rajadodve43@gmail.com>
2025-08-10 13:02:37 +0000

    Add increment and decrement button in chapters read

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get "dashboard", to: "dashboard#index"
-  resources :media_items
+  resources :media_items do
+    member do
+      patch :increment_chapter
+      patch :decrement_chapter
+    end
+  end
   root "media_items#index"
 end
