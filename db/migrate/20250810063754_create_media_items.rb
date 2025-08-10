ebdf28e7dfdbe1f512c13ad4fce0a2062bcf30c9
raja <rajadodve43@gmail.com>
2025-08-10 06:38:22 +0000

    Create MediaItem model
    with field values,
    - title:string
    - description:text
    - status:string
    - rating:integer
    - category:references

---
+++ db/migrate/20250810063754_create_media_items.rb
@@ -0,0 +1,13 @@
+class CreateMediaItems < ActiveRecord::Migration[8.0]
+  def change
+    create_table :media_items do |t|
+      t.string :title
+      t.text :description
+      t.string :status
+      t.integer :rating
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
