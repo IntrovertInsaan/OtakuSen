a66085b83fe8274214589fd14cf82d1ce73d944c
raja <rajadodve43@gmail.com>
2025-08-15 14:42:26 +0000

    Add controller tests for dashboard, profiles, media_items and model tests for media_item, Remove unused explore controller test

--- test/controllers/explore_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class ExploreControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get explore_index_url
-    assert_response :success
-  end
-end
--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -1,8 +1,12 @@
 require "test_helper"
 
 class ProfilesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+  end
+
   test "should get show" do
-    get profiles_show_url
+    get profile_url(@user) # Use the correct URL helper and pass the user
     assert_response :success
   end
 end
