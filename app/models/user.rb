665219620da94f89b9b3e10140584484614f5cf4
raja <rajadodve43@gmail.com>
2025-08-17 20:28:36 +0000

    SetUp model associations between user and achievements

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,9 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorited_items, through: :favorites, source: :media_item
 
+  has_many :user_achievements, dependent: :destroy
+  has_many :achievements, through: :user_achievements
+
   has_one_attached :avatar
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }
