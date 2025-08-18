3b907cc38f2516612a206ba9b49377333eed99be
raja <rajadodve43@gmail.com>
2025-08-18 16:46:38 +0000

    Fix: admin dashboard was protected, the test was trying to access it but not logged in as admin , so added fixture data for creating an admin

--- test/controllers/admin/dashboard_controller_test.rb
+++ test/controllers/admin/dashboard_controller_test.rb
@@ -3,8 +3,15 @@
 require "test_helper"
 
 class Admin::DashboardControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @admin_user = users(:admin) # Use the new admin fixture
+    sign_in @admin_user          # Sign in as the admin
+  end
+
   test "should get index" do
-    get admin_dashboard_index_url
+    get admin_root_url
     assert_response :success
   end
 end
