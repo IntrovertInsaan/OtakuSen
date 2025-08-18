eae402fb5ee69b4075cf0ed5bd2c8e8d0cf56368
raja <rajadodve43@gmail.com>
2025-08-18 16:38:56 +0000

    Fix: profiles controller test were failing because of no user signed in, so added it

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -3,12 +3,15 @@
 require "test_helper"
 
 class ProfilesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
     @user = users(:one)
+    sign_in @user
   end
 
   test "should get show" do
-    get profile_url(@user) # Use the correct URL helper and pass the user
+    get profile_url(@user)
     assert_response :success
   end
 end
