7ec99e4891f4a9af16e1a6db2964e61cf052f371
raja <rajadodve43@gmail.com>
2025-08-19 15:19:02 +0000

    Generate an action cable channel

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,3 +5,4 @@ import "./lib/turbo_confirm" // <-- our override
 
 import "trix"
 import "@rails/actiontext"
+import "./channels"
---
+++ app/javascript/channels/index.js
@@ -0,0 +1,2 @@
+// Import all the channels to be used by Action Cable
+import "./forum_thread_channel"
