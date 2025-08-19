67a9a4a4d4ad64aa243e89df94ff6356d04d8efc
raja <rajadodve43@gmail.com>
2025-08-19 13:50:18 +0000

    Add notes controller tests and Modified notes controller to pass tests, and Add users registration controller tests

--- test/controllers/notes_controller_test.rb
+++ test/controllers/notes_controller_test.rb
@@ -3,7 +3,52 @@
 require "test_helper"
 
 class NotesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:one)
+    @media_item = media_items(:one)
+    @note = notes(:one)
+    @other_user_media_item = media_items(:three)
+    sign_in @user
+  end
+
+  test "should get index" do
+    get media_item_notes_url(@media_item)
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_media_item_note_url(@media_item)
+    assert_response :success
+  end
+
+  test "should create note" do
+    assert_difference("Note.count") do
+      post media_item_notes_url(@media_item), params: { note: { title: "New Arc" } }
+    end
+    assert_redirected_to media_item_notes_url(@media_item)
+  end
+
+  test "should get edit" do
+    get edit_media_item_note_url(@media_item, @note)
+    assert_response :success
+  end
+
+  test "should update note" do
+    patch media_item_note_url(@media_item, @note), params: { note: { title: "Updated Arc Title" } }
+    assert_redirected_to media_item_notes_url(@media_item)
+  end
+
+  test "should destroy note" do
+    assert_difference("Note.count", -1) do
+      delete media_item_note_url(@media_item, @note)
+    end
+    assert_redirected_to media_item_notes_url(@media_item)
+  end
+
+  test "should NOT access notes for another user's item" do
+    get media_item_notes_url(@other_user_media_item)
+    assert_redirected_to root_url
+  end
 end
