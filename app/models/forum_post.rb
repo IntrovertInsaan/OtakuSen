a475b977d56bc2e01fd9581802e066ff3deae996
raja <rajadodve43@gmail.com>
2025-08-19 15:44:49 +0000

    Fix: The Devise::MissingWarden in Forum error
    Error: Devise could not find the `Warden::Proxy` instance on your request environment.
    Make sure that your application is loading Devise and Warden as expected and that the `Warden::Manager` middleware is present in your middleware stack.
    If you are seeing this on one of your tests, ensure that your tests are either executing the Rails middleware stack or that your tests are using the `Devise::Test::ControllerHelpers` module to inject the `request.env['warden']` object for you.

--- app/models/forum_post.rb
+++ app/models/forum_post.rb
@@ -6,7 +6,7 @@ class ForumPost < ApplicationRecord
   # After a new post is saved, run this code
   after_create_commit do
     # Broadcast a Turbo Stream "append" action to the correct forum thread's channel.
-    # It will use a new partial to render the post.
-    broadcast_append_to [ forum_thread, :posts ], partial: "forum_posts/forum_post", locals: { forum_post: self }
+    # Pass the post's creator (user) as a local variable named `current_user`
+    broadcast_append_to [ forum_thread, :posts ], partial: "forum_posts/forum_post", locals: { forum_post: self, current_user: self.user }
   end
 end
