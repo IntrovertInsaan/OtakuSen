1f49f7d645dbd8d48ff30e1d82c723d47dab4d6c
raja <rajadodve43@gmail.com>
2025-08-19 15:21:29 +0000

    Broadcast new posts

--- app/models/forum_post.rb
+++ app/models/forum_post.rb
@@ -1,5 +1,12 @@
 class ForumPost < ApplicationRecord
   belongs_to :forum_thread
   belongs_to :user
-  has_rich_text :content # Use the WYSIWYG editor
+  has_rich_text :content
+
+  # After a new post is saved, run this code
+  after_create_commit do
+    # Broadcast a Turbo Stream "append" action to the correct forum thread's channel.
+    # It will use a new partial to render the post.
+    broadcast_append_to forum_thread, partial: "forum_posts/forum_post", locals: { forum_post: self }
+  end
 end
