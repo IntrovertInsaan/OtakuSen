3079e19365cae120cd1d59daf43116767e38d2ae
raja <rajadodve43@gmail.com>
2025-08-20 00:43:49 +0000

    Modified forum posts and forum threads controller

--- app/controllers/forum_posts_controller.rb
+++ app/controllers/forum_posts_controller.rb
@@ -1,14 +1,15 @@
 class ForumPostsController < ApplicationController
   before_action :set_forum_thread
+  before_action :authenticate_user!
 
   def create
     @forum_post = @forum_thread.forum_posts.build(forum_post_params)
     @forum_post.user = current_user
 
     if @forum_post.save
-      head :ok   # broadcast happens automatically via after_commit
+      head :ok   # broadcast handled automatically by after_create_commit
     else
-      flash.now[:alert] = "Message could not be posted."
+      flash[:alert] = "Message could not be posted."
       redirect_to @forum_thread
     end
   end
