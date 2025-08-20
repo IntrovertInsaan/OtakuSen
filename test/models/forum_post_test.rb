e035e462d6d36053a88f1a65005c037436b5bc3c
raja <rajadodve43@gmail.com>
2025-08-20 01:25:25 +0000

    Add models tests for forum_post and forum_thread, and add content presence true in forum_post model for test to pass

--- test/models/forum_post_test.rb
+++ test/models/forum_post_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class ForumPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create!(email: "user@example.com", password: "password", username: "testuser")
+    @forum_thread = ForumThread.create!(title: "Thread", user: @user)
+    @forum_post = ForumPost.new(content: "Hello world", forum_thread: @forum_thread, user: @user)
+  end
+
+  test "should be valid with content, user, and forum_thread" do
+    assert @forum_post.valid?
+  end
+
+  test "should require content" do
+    @forum_post.content = ""
+    assert_not @forum_post.valid?
+  end
+
+  test "should belong to forum_thread" do
+    assert_equal @forum_thread, @forum_post.forum_thread
+  end
+
+  test "should belong to user" do
+    assert_equal @user, @forum_post.user
+  end
 end
--- test/models/forum_thread_test.rb
+++ test/models/forum_thread_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class ForumThreadTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create!(email: "user@example.com", password: "password", username: "testuser")
+    @forum_thread = ForumThread.new(title: "Test Thread", user: @user)
+  end
+
+  test "should be valid with title and user" do
+    assert @forum_thread.valid?
+  end
+
+  test "should require title" do
+    @forum_thread.title = ""
+    assert_not @forum_thread.valid?
+  end
+
+  test "should belong to user" do
+    assert_equal @user, @forum_thread.user
+  end
+
+  test "should allow nested forum posts" do
+    post = @forum_thread.forum_posts.build(content: "First post", user: @user)
+    assert post.valid?
+    @forum_thread.forum_posts << post
+    assert_equal 1, @forum_thread.forum_posts.size
+  end
 end
