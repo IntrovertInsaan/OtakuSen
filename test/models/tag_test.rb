dbe639e7b60d708e333db9c96ad018fee6079771
raja <rajadodve43@gmail.com>
2025-08-21 14:40:46 +0000

    Add tags and taggings factories and their tests, fix name mistake of users factories

--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -3,7 +3,19 @@
 require "test_helper"
 
 class TagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be valid" do
+    tag = build(:tag)
+    assert tag.valid?
+  end
+
+  test "should require a name" do
+    tag = build(:tag, name: "")
+    assert_not tag.valid?
+  end
+
+  test "name should be unique" do
+    original_tag = create(:tag)
+    duplicate_tag = build(:tag, name: original_tag.name)
+    assert_not duplicate_tag.valid?
+  end
 end
