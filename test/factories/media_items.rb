6b7fbef29282bf256c86a38f25cce166bb397e74
raja <rajadodve43@gmail.com>
2025-08-21 14:17:17 +0000

    Update media_item model factories with faker and removed media_item and users fixtures

--- test/factories/media_items.rb
+++ test/factories/media_items.rb
@@ -2,9 +2,17 @@
 
 FactoryBot.define do
   factory :media_item do
-    title { "Test Item" }
-    status { "Planning" }
-    association :user     # Automatically uses the :user factory
-    association :category # Automatically uses the :category factory
+    # Use Faker to generate a random book title
+    title { Faker::Book.title }
+    # Use Faker to generate a random paragraph for the description
+    description { Faker::Lorem.paragraph }
+    # Pick a random status from the list
+    status { [ "Planning", "In Progress", "Completed" ].sample }
+    # Generate a random rating between 1 and 10
+    rating { rand(1..10) }
+
+    # Associations remain the same
+    association :user
+    association :category
   end
 end
