3f3914441bd95c27940c9ec8e735d8f782bd4800
raja <rajadodve43@gmail.com>
2025-08-21 14:40:46 +0000

    Add tags and taggings factories and their tests, fix name mistake of users factories

--- test/models/tagging_test.rb
+++ test/models/tagging_test.rb
@@ -3,7 +3,18 @@
 require "test_helper"
 
 class TaggingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be valid with a media item and a tag" do
+    tagging = build(:tagging)
+    assert tagging.valid?
+  end
+
+  test "should not be valid without a media item" do
+    tagging = build(:tagging, media_item: nil)
+    assert_not tagging.valid?
+  end
+
+  test "should not be valid without a tag" do
+    tagging = build(:tagging, tag: nil)
+    assert_not tagging.valid?
+  end
 end
