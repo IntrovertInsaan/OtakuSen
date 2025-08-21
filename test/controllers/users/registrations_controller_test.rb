80275eb1b4f0e2e43963d280a6f3161c6383c4fb
raja <rajadodve43@gmail.com>
2025-08-21 17:34:36 +0000

    Add users/registrations controller test

--- test/controllers/users/registrations_controller_test.rb
+++ test/controllers/users/registrations_controller_test.rb
@@ -6,49 +6,63 @@ class Users::RegistrationsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
   setup do
-    @user = users(:one)
-    sign_in @user
+    # ARRANGE: Create a user with our factory
+    @user = create(:user)
+    
+    # ARRANGE: Sign in the user by simulating a login request
+    post user_session_url, params: { user: { email: @user.email, password: @user.password } }
   end
 
-  test "should get edit profile page" do
+  test "should get the edit profile page" do
+    # ACT
     get edit_user_registration_url
+    
+    # ASSERT
     assert_response :success
   end
 
-  test "should update username and bio without current password" do
+  test "should update username and bio without requiring the current password" do
+    # ACT: Send an update request for non-sensitive data with a blank current_password
     patch user_registration_url, params: {
       user: {
         username: "NewUsername",
         bio: "This is my new bio.",
-        current_password: "" # Intentionally blank
+        current_password: ""
       }
     }
+    
+    # ASSERT: Check for a successful redirect and that the data was updated
     assert_redirected_to root_url
     @user.reload
     assert_equal "NewUsername", @user.username
     assert_equal "This is my new bio.", @user.bio
   end
 
-  test "should NOT update password without current password" do
+  test "should NOT update password without the correct current password" do
+    # ACT: Send an update request for the password with a blank current_password
     patch user_registration_url, params: {
       user: {
         password: "newpassword",
         password_confirmation: "newpassword",
-        current_password: "" # Intentionally blank
+        current_password: ""
       }
     }
-    # Asserts that the page re-renders with an error, not a redirect
+    
+    # ASSERT: Check that the page re-renders with an error
     assert_response :unprocessable_content
   end
 
-  test "should update password WITH current password" do
+  test "should update password when the correct current password is provided" do
+    # ACT: Send an update request for the password with the correct current_password
     patch user_registration_url, params: {
       user: {
         password: "newpassword",
         password_confirmation: "newpassword",
-        current_password: "password123" # The correct password from fixtures
+        current_password: @user.password # Use the password from the factory
       }
     }
+    
+    # ASSERT: Check for a successful redirect
     assert_redirected_to root_url
   end
 end
