b373cdb7cb7f8ef0c05fb23835f1363c20b68318
raja <rajadodve43@gmail.com>
2025-08-21 14:10:01 +0000

    Add missing attributes that we missed in factories by replacing fixtures

--- test/factories/user.rb
+++ test/factories/user.rb
@@ -6,5 +6,12 @@ FactoryBot.define do
     sequence(:username) { |n| "testuser#{n}" }
     sequence(:email) { |n| "user#{n}@example.com" }
     password { "password123" }
+    password_confirmation { "password123" }
+
+    # A "trait" is a modifier that lets you create variations of a factory.
+    # In this case, it creates an admin user.
+    trait :admin do
+      admin { true }
+    end
   end
 end
