3fa476d4fb95e9d5f19350ab40ff3dda5a3ce6ac
raja <rajadodve43@gmail.com>
2025-08-21 17:05:34 +0000

    Add profiles and dashboard controller tests

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -3,15 +3,34 @@
 require "test_helper"
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
-  include Devise::Test::IntegrationHelpers # Include devise helpers
+  include Devise::Test::IntegrationHelpers
 
   setup do
-    @user = users(:one)
-    sign_in @user # Sign in a user before each test
+    # ARRANGE: Create a user from our factory
+    @user = create(:user)
+
+    # This signs in the user by simulating a form submission.
+    # It is more reliable than the direct `sign_in` helper.
+    post user_session_url, params: { user: { email: @user.email, password: @user.password } }
   end
 
-  test "should get index" do
-    get dashboard_url # Use the correct URL helper
+  test "should get index and display correct stats" do
+    # ARRANGE: Create unique data using our factories
+    category1 = create(:category) # Let the factory generate a unique name
+    category2 = create(:category)
+
+    create_list(:media_item, 3, user: @user, category: category1, status: "Completed")
+    create_list(:media_item, 2, user: @user, category: category2, status: "In Progress")
+
+    # ACT: Visit the dashboard page
+    get dashboard_url
+
+    # ASSERT: Check that the page loads successfully
     assert_response :success
+
+    # ASSERT: Check that the correct data is displayed in the stat cards
+    assert_select ".stat-value", text: "5"
+    assert_select "li", text: /#{category1.name}\s+3/
+    assert_select "li", text: /#{category2.name}\s+2/
   end
 end
--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -6,12 +6,25 @@ class ProfilesControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
   setup do
-    @user = users(:one)
-    sign_in @user
+    @user = create(:user)
+    # This signs in the user by simulating a form submission.
+    # It is more reliable than the direct `sign_in` helper.
+    post user_session_url, params: { user: { email: @user.email, password: @user.password } }
   end
 
-  test "should get show" do
+  test "should get show page for an existing user" do
     get profile_url(@user)
     assert_response :success
+  end
+
+  test "should get achievements page for an existing user" do
+    get achievements_profile_url(@user)
+    assert_response :success
+  end
+
+  test "should render not_found page for a non-existent user" do
+    non_existent_user_id = User.last.id + 1
+    get profile_url(id: non_existent_user_id)
+    assert_response :not_found
   end
 end
