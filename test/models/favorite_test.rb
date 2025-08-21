39788b6c72f4785167540fe4c580112cd0bfc074
raja <rajadodve43@gmail.com>
2025-08-21 14:49:13 +0000

    Add factories for notes, achievements, user_achievements, favorites and tests for favorite and note

--- test/models/favorite_test.rb
+++ test/models/favorite_test.rb
@@ -3,7 +3,18 @@
 require "test_helper"
 
 class FavoriteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be valid with a user and media item" do
+    favorite = build(:favorite)
+    assert favorite.valid?
+  end
+
+  test "should be invalid without a user" do
+    favorite = build(:favorite, user: nil)
+    assert_not favorite.valid?
+  end
+
+  test "should be invalid without a media item" do
+    favorite = build(:favorite, media_item: nil)
+    assert_not favorite.valid?
+  end
 end
--- test/models/note_test.rb
+++ test/models/note_test.rb
@@ -3,7 +3,13 @@
 require "test_helper"
 
 class NoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be valid with a media item" do
+    note = build(:note)
+    assert note.valid?
+  end
+
+  test "should be invalid without a media item" do
+    note = build(:note, media_item: nil)
+    assert_not note.valid?
+  end
 end
