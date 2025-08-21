d611a53417a90f488402847ddabd24478114cdb1
raja <rajadodve43@gmail.com>
2025-08-21 21:52:24 +0000

    Add more crud actions in forum posts controller and set authorization

--- app/controllers/forum_posts_controller.rb
+++ app/controllers/forum_posts_controller.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class ForumPostsController < ApplicationController
-  before_action :set_forum_thread
   before_action :authenticate_user!
+  before_action :set_forum_thread
+  before_action :set_forum_post, only: [ :edit, :update, :destroy ]
+  before_action :authorize_user!, only: [ :edit, :update, :destroy ]
 
   def create
     @forum_post = @forum_thread.forum_posts.build(forum_post_params)
@@ -11,15 +13,40 @@ class ForumPostsController < ApplicationController
     if @forum_post.save
       head :ok   # broadcast handled automatically by after_create_commit
     else
-      flash[:alert] = "Message could not be posted."
-      redirect_to @forum_thread
+      render json: { error: "Message could not be posted." }, status: :unprocessable_content
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @forum_post.update(forum_post_params)
+      redirect_to @forum_thread, notice: "Reply updated."
+    else
+      render :edit, status: :unprocessable_content
+    end
+  end
+
+  def destroy
+    @forum_post.destroy
+    redirect_to @forum_thread, notice: "Reply deleted."
   end
 
   private
 
   def set_forum_thread
     @forum_thread ||= ForumThread.find(params[:forum_thread_id])
+  end
+
+  # Finds the specific post
+  def set_forum_post
+    @forum_post = @forum_thread.forum_posts.find(params[:id])
+  end
+
+  # Verifies the user is the owner of the post
+  def authorize_user!
+    redirect_to @forum_thread, alert: "Not authorized." unless @forum_post.user == current_user
   end
 
   def forum_post_params
