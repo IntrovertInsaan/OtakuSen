e319b371dad239aa7f13c85429fc4738752a8230
raja <rajadodve43@gmail.com>
2025-08-21 18:20:37 +0000

    Add forum posts and threads controller and fix one error on forum threads index page

--- test/controllers/forum_threads_controller_test.rb
+++ test/controllers/forum_threads_controller_test.rb
@@ -3,7 +3,51 @@
 require "test_helper"
 
 class ForumThreadsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = create(:user)
+    @forum_thread = create(:forum_thread, user: @user)
+  end
+
+  test "should get the forum index" do
+    get forum_threads_url
+    assert_response :success
+  end
+
+  test "should show a forum thread" do
+    get forum_thread_url(@forum_thread)
+    assert_response :success
+  end
+
+  test "should not get new thread page if not signed in" do
+    get new_forum_thread_url
+    assert_redirected_to new_user_session_url
+  end
+
+  test "should get new thread page when signed in" do
+    sign_in @user
+    get new_forum_thread_url
+    assert_response :success
+  end
+
+  test "should create a forum thread with an initial post" do
+    sign_in @user
+
+    assert_difference("ForumThread.count") do
+      assert_difference("ForumPost.count") do
+        post forum_threads_url, params: {
+          forum_thread: {
+            title: "New Discussion Title",
+            forum_posts_attributes: [
+              { content: "This is the first post." }
+            ]
+          }
+        }
+      end
+    end
+
+    assert_redirected_to forum_thread_url(ForumThread.last)
+    assert_equal "Discussion started!", flash[:notice]
+  end
 end
