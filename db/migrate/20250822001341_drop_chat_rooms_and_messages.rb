0ffc1c2616720afae908f7924f1b0f1addff157e
raja <rajadodve43@gmail.com>
2025-08-22 00:18:20 +0000

    Removed chat_rooms and messages table that were created by feature branches

---
+++ db/migrate/20250822001341_drop_chat_rooms_and_messages.rb
@@ -0,0 +1,15 @@
+class DropChatRoomsAndMessages < ActiveRecord::Migration[8.0]
+  def change
+    drop_table :messages do |t|
+      t.text :content
+      t.references :user, foreign_key: true
+      t.references :chat_room, foreign_key: true
+      t.timestamps
+    end
+
+    drop_table :chat_rooms do |t|
+      t.string :name
+      t.timestamps
+    end
+  end
+end
