e79cdfdc40796500411ba78eb222912da84c44ec
raja <rajadodve43@gmail.com>
2025-08-22 00:18:20 +0000

    Removed chat_rooms and messages table that were created by feature branches

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,16 @@
-ActiveRecord::Schema[8.0].define(version: 2025_08_19_143352) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[8.0].define(version: 2025_08_22_001341) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_trgm"
@@ -79,6 +91,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_19_143352) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "posts_count", default: 0
+    t.datetime "last_post_at"
+    t.boolean "locked", default: false
+    t.boolean "pinned", default: false
+    t.integer "view_count", default: 0
     t.index ["user_id"], name: "index_forum_threads_on_user_id"
   end
 
