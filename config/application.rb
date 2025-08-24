fb464221d6bb5d38f67d9a484513c727e5d9c8f5
raja <rajadodve43@gmail.com>
2025-08-24 21:46:20 +0000

    Fix Rails config for bun and conditional database migration

--- config/application.rb
+++ config/application.rb
@@ -9,5 +9,6 @@ module OtakuSen
   class Application < Rails::Application
     config.load_defaults 8.0
     config.autoload_lib(ignore: %w[assets tasks])
+    config.javascript_build_runner = "bun"
   end
 end
