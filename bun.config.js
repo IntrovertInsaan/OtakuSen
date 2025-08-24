3d760218f8fb2f1ff8d226d50950bc3836e736d9
raja <rajadodve43@gmail.com>
2025-08-24 21:58:24 +0000

    Skip Rails JS build, build JS manually with bun

--- bun.config.js
+++ bun.config.js
@@ -1,37 +1,12 @@
-import path from 'path';
-import fs from 'fs';
+import { build } from "bun";
 
-const config = {
-  sourcemap: "external",
-  entrypoints: ["app/javascript/application.js"],
-  outdir: path.join(process.cwd(), "app/assets/builds"),
-};
+await build({
+  entrypoints: ["./app/javascript/application.js"],
+  outdir: "./app/assets/builds",
+  target: "browser",
+  format: "esm",
+  minify: true,
+  sourcemap: "external"
+});
 
-const build = async (config) => {
-  const result = await Bun.build(config);
-
-  if (!result.success) {
-    if (process.argv.includes('--watch')) {
-      console.error("Build failed");
-      for (const message of result.logs) {
-        console.error(message);
-      }
-      return;
-    } else {
-      throw new AggregateError(result.logs, "Build failed");
-    }
-  }
-};
-
-(async () => {
-  await build(config);
-
-  if (process.argv.includes('--watch')) {
-    fs.watch(path.join(process.cwd(), "app/javascript"), { recursive: true }, (eventType, filename) => {
-      console.log(`File changed: ${filename}. Rebuilding...`);
-      build(config);
-    });
-  } else {
-    process.exit(0);
-  }
-})();
+console.log("Build complete!");
