882fbf9d84092051ae2b43621ef4c0f10ed14511
raja <rajadodve43@gmail.com>
2025-08-27 12:16:23 +0000

    Revert category model so now if category gets deleted so media items too

--- db/migrate/20250827114824_change_category_id_to_nullable_in_media_items.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class ChangeCategoryIdToNullableInMediaItems < ActiveRecord::Migration[8.0]
-  def change
-    change_column_null :media_items, :category_id, true
-  end
-end
