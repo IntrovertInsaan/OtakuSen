a8262fe005354433db05d4f498be9636709baebe
raja <rajadodve43@gmail.com>
2025-08-27 12:20:24 +0000

    Fix devise error could not find a valid mapping for User

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,9 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
+# Add this line to configure Devise for tests
+Devise.mappings[:user]
+
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
