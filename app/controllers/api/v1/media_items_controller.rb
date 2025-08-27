c10b62b7e28b30aaf19ab79ca747faeb6e411fa6
raja <rajadodve43@gmail.com>
2025-08-27 10:08:21 +0000

    Create the API controller

---
+++ app/controllers/api/v1/media_items_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::MediaItemsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @media_items = current_user.media_items.order(created_at: :desc)
+  end
+
+  def show
+    @media_item = current_user.media_items.find(params[:id])
+  end
+end
