45fd148dfc7ff3d7b458f68ec85a3f8bf83fa239
raja <rajadodve43@gmail.com>
2025-08-27 12:16:23 +0000

    Revert category model so now if category gets deleted so media items too

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Category < ApplicationRecord
-  has_many :media_items, dependent: :nullify
+  has_many :media_items, dependent: :destroy
   validates :name, presence: true, uniqueness: true
 end
