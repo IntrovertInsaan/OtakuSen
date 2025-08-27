105b8374ce210c749607f74207d00f23147f4e3b
raja <rajadodve43@gmail.com>
2025-08-27 10:06:32 +0000

    Create the API routes

--- config/routes.rb
+++ config/routes.rb
@@ -34,4 +34,10 @@ Rails.application.routes.draw do
   resources :forum_threads, only: [ :index, :show, :new, :create ] do
     resources :forum_posts, only: [ :create, :edit, :update, :destroy ]
   end
+
+  namespace :api do
+    namespace :v1 do
+      resources :media_items, only: [ :index, :show ]
+    end
+  end
 end
