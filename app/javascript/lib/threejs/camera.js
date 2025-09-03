4ad2486a5f5264c81ba5d00fc1725fbdca043974
raja <rajadodve43@gmail.com>
2025-09-03 15:52:50 +0000

    Create the pointerlock controls and grid helpers

--- app/javascript/lib/threejs/camera.js
+++ app/javascript/lib/threejs/camera.js
@@ -2,6 +2,6 @@ import * as THREE from 'three';
 
 export function createCamera() {
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  camera.position.z = 5;
+  camera.position.set(0, 10, 30);
   return camera;
 }
