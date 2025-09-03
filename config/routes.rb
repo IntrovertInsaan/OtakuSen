9e19d8ed64e878839947eacb23a99726bea24281
raja <rajadodve43@gmail.com>
2025-09-03 11:20:52 +0000

    Create pages controller with index where threejs scene will be displayed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get "pages/index"
   devise_for :users, controllers: { registrations: "users/registrations" }
 
   root "media_items#index"
