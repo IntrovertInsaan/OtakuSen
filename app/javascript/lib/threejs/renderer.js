c27e8e87a78ab7ec900c692499c290aa698804b4
raja <rajadodve43@gmail.com>
2025-09-03 17:22:22 +0000

    feat: Add lights, objects, and shadows to the scene

--- app/javascript/lib/threejs/renderer.js
+++ app/javascript/lib/threejs/renderer.js
@@ -1,8 +1,13 @@
 import * as THREE from 'three';
 
 export function createRenderer(element) {
-  const renderer = new THREE.WebGLRenderer({ alpha: true });
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
   element.appendChild(renderer.domElement);
+
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
   return renderer;
 }
